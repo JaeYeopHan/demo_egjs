fb8415bf009e6e599c8dfac70e22a6e895b24fde
JaeYeopHan <ljyhanll@gmail.com>
2017-07-08 15:24:50 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import '../public/main.css';
+import '../public/index.css';
+import MovableCoord from "@egjs/movablecoord";
+
+const el = document.querySelector('#area');
+const instance = new MovableCoord("#area", {
+	max : [300, 400]
+}).bind(el, {
+	direction : MovableCoord.DIRECTION_ALL,
+	scale: [1, 1.5]
+});
